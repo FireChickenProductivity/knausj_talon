11f142a711bf0037fd132e452835baa41334f2a1
Fire Chicken <firechickenproductivity@gmail.com>
2026-09-24 17:26:52 -0600

    Support screen number

--- core/help/command_menu.py
+++ core/help/command_menu.py
@@ -1,6 +1,7 @@
 from talon import Module, Context, actions
 
 import egui
+from egui import Mutable
 from talon.egui import Window
 import skia
 
@@ -12,22 +13,6 @@ class Page:
 	ui: Callable
 	title: str
 
-async def move_the_mouse_ui(ui: egui.ui) -> None:
-	ui.label("The mouse grid lets you move the mouse by dictating numbers. You use one of the below commands to open the grid. This divides the area you made the grid around into 9 rectangles. Picking one of the numbers recreates the grid within that rectangle and moves the mouse to the center of that rectangle")
-	ui.add_space(10)
-	if ui.button("mouse grid").clicked():
-		actions.user.grid_select_screen(1)
-		actions.user.grid_activate()
-	ui.label("Opens the mouse grid on the main screen")
-	if ui.button("grid win").clicked():
-		actions.user.grid_place_window()
-		actions.user.grid_activate()
-	ui.label("Opens the mouse grid around the currently focused window")
-	ui.add_space(10)
-	if ui.button("grid close").clicked():
-		actions.user.grid_close()
-
-
 class CommandMenu:
 	def __init__(self):
 		self.window = Window()
@@ -37,7 +22,7 @@ class CommandMenu:
 		self.window.rect = skia.Rect(x=10, y=20, width=400, height=600)
 		self.window.set_content(self.ui)
 		self.pages = [
-			Page(move_the_mouse_ui, "Move the mouse (voice commands)"),
+			Page(self.move_the_mouse_ui, "Move the mouse (voice commands)"),
 			Page(None, "Move the mouse (eye tracking)"),
 			Page(None, "Click"),
 			Page(None, "Scroll the mouse"),
@@ -45,6 +30,7 @@ class CommandMenu:
 			Page(None, "Move the cursor"),
 			Page(None, "Type text")
 		]
+		self.grid_screen_number = Mutable(2)
 
 	async def ui(self, ui: egui.ui) -> None:
 		if self.current_page is None:
@@ -53,12 +39,41 @@ class CommandMenu:
 			for i, page in enumerate(self.pages):
 				if ui.button(page.title).clicked():
 					self.current_page = i
+			ui.add_space(10)
 		else:
 			page = self.pages[self.current_page]
 			await page.ui(ui)
-		ui.add_space(10)
+			ui.add_space(10)
+			if ui.button("command menu").clicked():
+				self.current_page = None
 		if ui.button("Command menu close").clicked():
 			self.hide()
+
+	async def move_the_mouse_ui(self, ui: egui.ui) -> None:
+		ui.label("The mouse grid lets you move the mouse by dictating numbers. You use one of the below commands to open the grid. This divides the area you made the grid around into 9 rectangles. Picking one of the numbers recreates the grid within that rectangle and moves the mouse to the center of that rectangle")
+		ui.add_space(10)
+		if ui.button("mouse grid").clicked():
+			actions.user.grid_select_screen(1)
+			actions.user.grid_activate()
+		ui.label("Opens the mouse grid on the main screen")
+		if ui.button("grid win").clicked():
+			actions.user.grid_place_window()
+			actions.user.grid_activate()
+		ui.label("Opens the mouse grid around the currently focused window")
+		async with ui.horizontal():
+			if ui.button("grid screen").clicked():
+				screen_number = self.grid_screen_number.get()
+				actions.user.grid_select_screen(screen_number)
+				actions.user.grid_activate()
+			ui.label("(and then you say a screen number): ")
+			ui.add(egui.DragValue(self.grid_screen_number))
+			if self.grid_screen_number.get() < 1:
+				self.grid_screen_number.set(1)
+		ui.label("Opens the mouse grid around the given screen")
+		ui.add_space(10)
+		if ui.button("grid close").clicked():
+			actions.user.grid_close()
+		ui.label("Closes the mouse grid")
 
 	def show(self) -> None:
 		self.window.show()
