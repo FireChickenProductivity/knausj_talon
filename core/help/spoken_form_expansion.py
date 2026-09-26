1ef06712567d99ecfa3c8457690a3d545e8b8375
Fire Chicken <firechickenproductivity@gmail.com>
2026-09-25 20:42:06 -0600

    Make work

--- core/help/spoken_form_expansion.py
+++ core/help/spoken_form_expansion.py
@@ -1,4 +1,4 @@
-from talon import Module, Context, actions
+from talon import Module, Context, actions, registry
 
 import egui
 from egui import Mutable
@@ -20,8 +20,9 @@ class SpokenForm:
 
 	async def ui(self, ui, path):
 		ui.strong(f"{self.type_name}: {self.text}")
-		if self.get_description:
-			ui.label(self.get_description)
+		description = self.get_description()
+		if description:
+			ui.label(description)
 		rules = compute_relevant_rules_for_text(self.text)
 		if rules:
 			ui.separator()
@@ -51,20 +52,35 @@ class SpokenForm:
 			
 				
 def get_list_description(name):
-	pass
+	return registry.decls.lists[name].desc
 
 def get_list_contents(name):
-	pass
+	try:
+		return actions.user.talon_get_active_registry_list(name).items()
+	except Exception as ex:
+		return []
 
-def compute_spoken_forms(text):
+def get_capture_description(name):
+	return registry.decls.captures[name].desc
+
+def get_capture_rule(name):
+	capture = registry.captures[name][-1]
+	rule = capture.rule
+	return rule.rule
+
+def compute_spoken_forms(text, exclude_total=True):
+	text = text.strip()
 	forms = []
 	start = None
 	for i, c in enumerate(text):
+		if exclude_total and i == len(text) - 1 and start == 0:
+			return []
 		if c in ("{", "<"):
 			start = i
+			print('start', start)
 		elif c == "}":
 			form_text = text[start:i+1]
-			list_name = text[1:-1]
+			list_name = form_text[1:-1]
 			form = SpokenForm(
 				"list",
 				form_text,
@@ -72,6 +88,19 @@ def compute_spoken_forms(text):
 				lambda : get_list_contents(list_name)
 			)
 			forms.append(form)
+			start = None
+		elif c == ">":
+			form_text = text[start:i+1]
+			capture_name = form_text[1:-1]
+			form = SpokenForm(
+				"capture",
+				get_capture_rule(capture_name),
+				lambda : get_capture_description(capture_name),
+				lambda : []
+			)
+			forms.append(form)
+			start = None
+	return forms
 
 
 
@@ -81,6 +110,7 @@ class ExpansionDemo:
 		self.window.toplevel = True
 		self.window.draggable = True
 		self.window.decorated = False
+		self.window.set_content(self.ui)
 		self.path = []
 
 	async def ui(self, ui):
@@ -96,3 +126,6 @@ class ExpansionDemo:
 
 	def hide(self):
 		self.window.hide()
+
+demo = ExpansionDemo()
+demo.show(compute_spoken_forms("<user.keys>", exclude_total=False)[-1])
